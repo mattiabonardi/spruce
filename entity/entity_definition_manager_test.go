a6d02fcb2f5fa3a650ce0ed27c2a55483f9fd400
mattiabonardi <mattiabonardi99@gmail.com>
2023-07-07 01:10:52 +0200

    Add test

--- entity/entity_definition_manager_test.go
+++ entity/entity_definition_manager_test.go
@@ -1,19 +1,14 @@
 package entity
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/mattiabonardi/spruce/models"
-	"gopkg.in/yaml.v2"
 )
 
 func TestGetDefinition(t *testing.T) {
 	entityDefinitionManager := EntityDefinitionManager{}
 	entityDefinition := entityDefinitionManager.GetDefinition("gender")
-
-	yamlData, _ := yaml.Marshal(entityDefinition)
-	fmt.Println(string(yamlData))
 
 	if entityDefinition.DataSource.Type != models.YamlFile {
 		t.Fatal("Aspected: " + models.YamlFile + " got: " + entityDefinition.DataSource.Type)
---
+++ entity/entity_manager_test.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mattiabonardi/spruce/models"
+)
+
+func TestGetAll(t *testing.T) {
+	entityManager := EntityManager{}
+	entities := entityManager.GetAll("gender", models.EntityContext{})
+
+	if entities[0].Attributes["id"].Type != "String" {
+		t.Fatal("Expected: String got: " + entities[0].Attributes["id"].Type)
+	}
+
+	if entities[0].Attributes["id"].Value != "1" {
+		t.Fatal("Expected: 1 got: " + fmt.Sprintf("%v", entities[0].Attributes["id"].Value))
+	}
+
+	if entities[0].Attributes["description"].Value != "male" {
+		t.Fatal("Expected: male got: " + fmt.Sprint(entities[0].Attributes["description"].Value))
+	}
+
+	if len(entities) == 2 {
+		t.Fatal("Expected: 2 got: " + fmt.Sprint(entities))
+	}
+}
